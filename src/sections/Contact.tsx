ebcf71156edac8137f0da3562d53a3f131e40a02
unknown <ezequielmadrid>
2024-11-19 20:27:50 -0300

    contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -2,67 +2,39 @@ import { SectionHeader } from "@/components/SectionHeader";
 
 export const ContactSection = () => {
   return (
-    <section className="py-14">
+    <section className="h-screen py-14">
       <SectionHeader
         title={"Contact me"}
         eyebrow={"Let's work together!"}
         description={"📲"}
       />
-      <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi odit,
-        perferendis in ex sit officiis, optio impedit obcaecati quod quaerat,
-        vel dignissimos dolor. Odio quod doloremque autem consectetur veniam.
-      </article>
-      <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi odit,
-        perferendis in ex sit officiis, optio impedit obcaecati quod quaerat,
-        vel dignissimos dolor. Odio quod doloremque autem consectetur veniam.
-      </article>
-      <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi odit,
-        perferendis in ex sit officiis, optio impedit obcaecati quod quaerat,
-        vel dignissimos dolor. Odio quod doloremque autem consectetur veniam.
-      </article>
-      <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi odit,
-        perferendis in ex sit officiis, optio impedit obcaecati quod quaerat,
-        vel dignissimos dolor. Odio quod doloremque autem consectetur veniam.
-      </article>
-      <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi odit,
-        perferendis in ex sit officiis, optio impedit obcaecati quod quaerat,
-        vel dignissimos dolor. Odio quod doloremque autem consectetur veniam.
-      </article>
-      <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi odit,
-        perferendis in ex sit officiis, optio impedit obcaecati quod quaerat,
-        vel dignissimos dolor. Odio quod doloremque autem consectetur veniam.
-      </article>
-      <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi odit,
-        perferendis in ex sit officiis, optio impedit obcaecati quod quaerat,
-        vel dignissimos dolor. Odio quod doloremque autem consectetur veniam.
-      </article>
-      <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi odit,
-        perferendis in ex sit officiis, optio impedit obcaecati quod quaerat,
-        vel dignissimos dolor. Odio quod doloremque autem consectetur veniam.
-      </article>
-      <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi odit,
-        perferendis in ex sit officiis, optio impedit obcaecati quod quaerat,
-        vel dignissimos dolor. Odio quod doloremque autem consectetur veniam.
-      </article>
-      <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi odit,
-        perferendis in ex sit officiis, optio impedit obcaecati quod quaerat,
-        vel dignissimos dolor. Odio quod doloremque autem consectetur veniam.
-      </article>
-      <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga modi odit,
-        perferendis in ex sit officiis, optio impedit obcaecati quod quaerat,
-        vel dignissimos dolor. Odio quod doloremque autem consectetur veniam.
-      </article>
+      <div className="overflow-hidden">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10 p-4">
+          <article className="p-6 rounded-lg transform hover:scale-105 transition duration-300 hover:shadow-lg shadow-md bg-emerald-950 shadow-slate-600">
+            <h3 className="text-lg font-bold">Get in Touch</h3>
+            <p className="text-gray-400 mt-2">
+              Feel free to send a message or connect via social platforms.
+            </p>
+          </article>
+          <article className="p-6 rounded-lg transform hover:scale-105 transition duration-300 hover:shadow-lg shadow-md bg-emerald-950 shadow-slate-600">
+            <h3 className="text-lg font-bold">Location</h3>
+            <p className="text-gray-400 mt-2">
+              {" "}
+              x¿ Based in the heart of innovation.
+            </p>
+          </article>
+          <article className="p-6 rounded-lg transform hover:scale-105 transition duration-300 hover:shadow-lg shadow-md bg-emerald-950 shadow-slate-600">
+            <h3 className="text-lg font-bold">Socials</h3>
+            <p className="text-gray-400 mt-2">
+              Follow me on LinkedIn, GitHub, and Twitter for updates.
+            </p>
+          </article>
+          <article className="p-6 rounded-lg transform hover:scale-105 transition duration-300 hover:shadow-lg shadow-md bg-emerald-950 shadow-slate-600">
+            <h3 className="text-lg font-bold">Email</h3>
+            <p className="text-gray-400 mt-2">contact@example.com</p>
+          </article>
+        </div>
+      </div>
     </section>
   );
 };
